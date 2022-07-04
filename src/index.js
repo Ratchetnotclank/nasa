eb96c399fd4edacae6c5213a94b97d6544b451e5
Ratchetnotclank <108676875+Ratchetnotclank@users.noreply.github.com>
2022-07-04 18:08:40 +0200

    Refactor code to use async/await instead of promises

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 const nasaKey = process.env.NASA_KEY
 
+// Get daily image
+
 myFunction();
-function myFunction() {
+async function myFunction() {
     const dailyImageUrl = `https://api.nasa.gov/planetary/apod?api_key=${nasaKey}`;
 
     const imageElement = document.getElementById('dailyImage');
     const imageTitleElement = document.getElementById('dailyImageTitle')
 
-    fetch(dailyImageUrl)
-        .then(response => response.json())
-        .then(jsonResponse => {
-            console.log(jsonResponse);
-            const imageUrl = jsonResponse.url;
-            const imageTitle = jsonResponse.title;
-            imageElement.src = imageUrl;
-            imageElement.alt = imageTitle;
-            imageTitleElement.textContent = imageTitle;
-        });
+
+    const response = await fetch(dailyImageUrl);
+    const jsonResponse = await response.json();
+    const imageUrl = jsonResponse.url;
+    const imageTitle = jsonResponse.title;
+    imageElement.src = imageUrl;
+    imageElement.alt = imageTitle;
+    imageTitleElement.textContent = imageTitle;
 }
